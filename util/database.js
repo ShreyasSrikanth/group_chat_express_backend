670b7d736b728e9856dce1ba129f2e8c4ec66731
Shreyas Srikanth <sshreyas567@gmail.com>
2024-01-13 01:05:24 +0530

    updated db

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelise = new Sequelize("group-chat","root","root",{
+const sequelise = new Sequelize(process.env.DB_SCHEMA_NAME,process.env.DB_ROOT_USER,process.env.DB_ROOT_PASS,{
     dialect: 'mysql',
-    host: "localhost",
+    host: process.env.DB_HOST,
     timezone: '+05:30'
 });
 
