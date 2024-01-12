9c291b3c070365327a32bce4707ec57037f7146b
Shreyas Srikanth <sshreyas567@gmail.com>
2024-01-12 17:29:09 +0530

    bug fix in usergroups model

--- models/usergroupModel.js
+++ models/usergroupModel.js
@@ -2,12 +2,6 @@ const sequelize = require('sequelize')
 const database = require('../util/database');
 
 const usergroups = database.define('usergroups',{
-    idd:{
-        type:sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
-    },
 });
 
 module.exports = usergroups;
