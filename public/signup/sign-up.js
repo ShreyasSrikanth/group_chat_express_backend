c4c947839f0627dd0b786e49cd645616b6065946
Shreyas Srikanth <sshreyas567@gmail.com>
2024-01-29 21:28:49 +0530

    changed url

--- public/signup/sign-up.js
+++ public/signup/sign-up.js
@@ -11,7 +11,7 @@ async function sendSignUpDetails(e){
     let pass = document.getElementById('password').value;
 
     try{
-        await axios.post('http://localhost:3000/users/signup', {
+        await axios.post('http://3.87.172.140:3000/users/signup', {
             name: name,
             email: email,
             phone: phone,
